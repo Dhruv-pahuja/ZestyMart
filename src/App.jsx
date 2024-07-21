7bdce34410eefb99ab424940564d233dc49a2604
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2024-07-21 14:34:59 +0530

    fixed state on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React ,{useState,useEffect} from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
 import { Outlet } from 'react-router'
 import { Header,Footer } from './components'
 
 function App() {
+  const [loading, setLoading] = useState(true)
   const theme = useSelector((state) => state.theme)
-  return (
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
+
+  return !loading ?(
     <div className={`${theme === 'dark' ? 'dark' : ''}`} style={{ minHeight: '100vh' }}>
     <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-[#111827] text-black dark:text-white">
       <Header />
@@ -15,7 +32,7 @@ function App() {
       <Footer />
     </div>
   </div>
-  )
+  ) : null
 }
 
 export default App
